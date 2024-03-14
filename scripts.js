824320c066666cd93b90a2e8e941b27c46d5abd2
anjosz1 <dosanjosbastosluan@gamil.com>
2024-03-14 09:48:52 -0300

    mensagem contém nome

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar, false);
+var nome = document.getElementById("nome");
 
 function alertar(event){
-    alert("você clicou no botão!!!!" + event.value);
+    alert("você clicou no botão!!!!" + nome.value);
 }
